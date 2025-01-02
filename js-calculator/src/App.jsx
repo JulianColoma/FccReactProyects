f91ebf4d6fc78f80373b5ab107305a466acdce9f
JulianColoma <juliancoloma751@gmail.com>
2025-01-02 19:44:11 -0300

    Completed js-calculator requirements

--- js-calculator/src/App.jsx
+++ js-calculator/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-
 function App() {
   const [result, setResult] = useState('0')
   const [solved, setSolved ]= useState(false)
@@ -8,9 +7,6 @@ function App() {
     let res = result
     let input = e.target.value
     const newres = validate(input, res)
-    console.log('--------------')
-    console.log(res)
-    console.log(newres)
     setResult(newres)
     setSolved(false)
   }
@@ -20,13 +16,26 @@ function App() {
     if(res == '' && input.match(/[+\.*/]/)) input = ''
     if(solved && !(input).match(/[+\-*/]/)) res = ''
     if(res == '0') res = ''
-    if(res[(res.length -1)]){
-      //if((res[(res.length -1)]).match(/[+\-*/]/) && input.match(/[+\-*/]/)) res[res.length -1] = input
-    }
     return (res + input)
   }
   const solver = () =>{
-    const res = result
+    let res = result
+      let parts = []
+      for (let i = res.length - 1; i >= 0; i--) {
+        if (res[i].match(/[+\*/]/) && res[i - 1] && res[i - 1].match(/[+\-*/]/)) {
+          parts.unshift(res[i]);
+          let j = 0;
+          let k = i - 1;
+          while (res[k] && res[k].match(/[+\-*/]/)) {
+            k--;
+            j++;
+          }
+          i -= j;
+        } else {
+          parts.unshift(res[i]);
+        }
+      }
+      res = parts.join('')
     let newres = eval(res) % 1 !== 0 ? round(eval(res)) : eval(res)
     setResult(newres.toString())
     setSolved(true)
@@ -46,25 +55,25 @@ function App() {
   return (
     <>
       <div id="display">{result}</div>
-      <div id="grid" onClick={handleClick}>
-        <button id="zero" value='0'>0</button>
-        <button id="one" value='1'>1</button>
-        <button id="two" value='2'>2</button>
-        <button id="three" value='3'>3</button>
-        <button id="four" value='4'>4</button>
-        <button id="five" value='5'>5</button>
-        <button id="six" value='6'>6</button>
-        <button id="seven" value='7'>7</button>
-        <button id="eight" value='8'>8</button>
-        <button id="nine" value='9'>9</button>
-        <button id="add" value='+'>+</button>
-        <button id="subtract" value='-'>-</button>
-        <button id="multiply" value='*'>x</button>
-        <button id="divide" value='/'>/</button>
-        <button id="decimal"value='.'>.</button>
+      <div id="grid">
+        <button id="zero" value='0' onClick={handleClick}>0</button>
+        <button id="one" value='1' onClick={handleClick}>1</button>
+        <button id="two" value='2'onClick={handleClick}>2</button>
+        <button id="three" value='3'onClick={handleClick}>3</button>
+        <button id="four" value='4'onClick={handleClick}>4</button>
+        <button id="five" value='5'onClick={handleClick}>5</button>
+        <button id="six" value='6'onClick={handleClick}>6</button>
+        <button id="seven" value='7'onClick={handleClick}>7</button>
+        <button id="eight" value='8'onClick={handleClick}>8</button>
+        <button id="nine" value='9' onClick={handleClick}>9</button>
+        <button id="add" value='+'onClick={handleClick}>+</button>
+        <button id="subtract" value='-'onClick={handleClick}>-</button>
+        <button id="multiply" value='*'onClick={handleClick}>x</button>
+        <button id="divide" value='/'onClick={handleClick}>/</button>
+        <button id="decimal"value='.'onClick={handleClick}>.</button>
+        <button id='clear'onClick={clear}>AC</button>
+        <button id='equals' onClick={solver}>=</button>
       </div>
-      <button id='clear'onClick={clear}>AC</button>
-      <button id='equals' onClick={solver}>=</button>
     </>
   )
 }
