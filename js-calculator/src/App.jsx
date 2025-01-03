c28b0f57d897de94279334f94d1fd2570825a88c
JulianColoma <juliancoloma751@gmail.com>
2025-01-03 20:06:00 -0300

    Basic js-calculator styling

--- js-calculator/src/App.jsx
+++ js-calculator/src/App.jsx
@@ -3,6 +3,17 @@ import './App.css'
 function App() {
   const [result, setResult] = useState('0')
   const [solved, setSolved ]= useState(false)
+  const handleKey = (e) =>{
+    const validChars = '1234567890+-*/'
+    if(e.key == 'Backspace' || e.key == 'Delete') clear()
+    if(validChars.includes((e.key).toString())){
+      let res = result
+      let input = e.key
+      const newres = validate(input, res)
+      setResult(newres)
+      setSolved(false)
+    }
+  }
   const handleClick = (e) => {
     let res = result
     let input = e.target.value
@@ -53,28 +64,29 @@ function App() {
   }
 
   return (
-    <>
+    <div tabIndex={0} onKeyDown={handleKey} className='container'
+    style={{ outline: "none", border: "none", padding: "10px" }}> 
       <div id="display">{result}</div>
-      <div id="grid">
-        <button id="zero" value='0' onClick={handleClick}>0</button>
-        <button id="one" value='1' onClick={handleClick}>1</button>
-        <button id="two" value='2'onClick={handleClick}>2</button>
-        <button id="three" value='3'onClick={handleClick}>3</button>
-        <button id="four" value='4'onClick={handleClick}>4</button>
-        <button id="five" value='5'onClick={handleClick}>5</button>
-        <button id="six" value='6'onClick={handleClick}>6</button>
-        <button id="seven" value='7'onClick={handleClick}>7</button>
-        <button id="eight" value='8'onClick={handleClick}>8</button>
-        <button id="nine" value='9' onClick={handleClick}>9</button>
-        <button id="add" value='+'onClick={handleClick}>+</button>
-        <button id="subtract" value='-'onClick={handleClick}>-</button>
-        <button id="multiply" value='*'onClick={handleClick}>x</button>
-        <button id="divide" value='/'onClick={handleClick}>/</button>
-        <button id="decimal"value='.'onClick={handleClick}>.</button>
+      <div id="grid" >
+        <button id="zero" className='number' value='0' onClick={handleClick}>0</button>
+        <button id="one" className='number'value='1' onClick={handleClick}>1</button>
+        <button id="two"className='number' value='2'onClick={handleClick}>2</button>
+        <button id="three" className='number'value='3'onClick={handleClick}>3</button>
+        <button id="four" className='number'value='4'onClick={handleClick}>4</button>
+        <button id="five"className='number' value='5'onClick={handleClick}>5</button>
+        <button id="six"className='number' value='6'onClick={handleClick}>6</button>
+        <button id="seven" className='number'value='7'onClick={handleClick}>7</button>
+        <button id="eight"className='number' value='8'onClick={handleClick}>8</button>
+        <button id="nine" className='number'value='9' onClick={handleClick}>9</button>
+        <button id="add"className='operator' value='+'onClick={handleClick}>+</button>
+        <button id="subtract"className='operator' value='-'onClick={handleClick}>-</button>
+        <button id="multiply" className='operator'value='*'onClick={handleClick}>x</button>
+        <button id="divide" className='operator'value='/'onClick={handleClick}>/</button>
+        <button id="decimal"className='operator'value='.'onClick={handleClick}>.</button>
         <button id='clear'onClick={clear}>AC</button>
         <button id='equals' onClick={solver}>=</button>
       </div>
-    </>
+    </div>
   )
 }
 
